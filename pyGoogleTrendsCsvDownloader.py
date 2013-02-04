da8f30d1e45464a257eaa0217be8caeb67339001
Chow Loong Jin <hyperair@debian.org>
2013-02-04 12:19:39 +0800

    Tidy up code to conform to PEP-8

--- pyGoogleTrendsCsvDownloader.py
+++ pyGoogleTrendsCsvDownloader.py
@@ -32,9 +32,9 @@ class pyGoogleTrendsCsvDownloader(object):
         All immutable system variables are also defined here
         '''
 
-        # The amount of time (in secs) that the script should wait before making a request.
-        # This can be used to throttle the downloading speed to avoid hitting servers too hard.
-        # It is further randomized.
+        # The amount of time (in secs) that the script should wait before
+        # making a request. This can be used to throttle the downloading speed
+        # to avoid hitting servers too hard. It is further randomized.
         self.download_delay = 0.25
 
         self.service = "trendspro"
@@ -42,14 +42,26 @@ class pyGoogleTrendsCsvDownloader(object):
         self.url_download = self.url_service + "trendsReport?"
 
         self.login_params = {}
-        # These headers are necessary, otherwise Google will flag the request at your account level
-        self.headers = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'),
-                        ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"),
+        # These headers are necessary, otherwise Google will flag the request
+        # at your account level
+        self.headers = [('User-Agent',
+                         'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) '
+                         'Gecko/20100101 Firefox/12.0'),
+                        ("Accept",
+                         "text/html,application/xhtml+xml,application/xml;"
+                         "q=0.9,*/*;q=0.8"),
                         ("Accept-Language", "en-gb,en;q=0.5"),
                         ("Accept-Encoding", "gzip, deflate"),
                         ("Connection", "keep-alive")]
-        self.url_login = 'https://accounts.google.com/ServiceLogin?service='+self.service+'&passive=1209600&continue='+self.url_service+'&followup='+self.url_service
-        self.url_authenticate = 'https://accounts.google.com/accounts/ServiceLoginAuth'
+        self.url_login = ('https://accounts.google.com/ServiceLogin?'
+                          'service={service}&'
+                          'passive=1209600&'
+                          'continue={url_service}&'
+                          'followup={url_service}') \
+                          .format(service=self.service,
+                                  url_service=self.url_service)
+        self.url_authenticate = ('https://accounts.google.com/accounts/'
+                                 'ServiceLoginAuth')
         self.header_dictionary = {}
 
         self._authenticate(username, password)
@@ -57,16 +69,24 @@ class pyGoogleTrendsCsvDownloader(object):
     def _authenticate(self, username, password):
         '''
         Authenticate to Google:
-        1 - make a GET request to the Login webpage so we can get the login form
-        2 - make a POST request with email, password and login form input values
+        1 - make a GET request to the Login webpage so we can get the login
+            form
+        2 - make a POST request with email, password and login form input
+            values
         '''
 
         # Make sure we get CSV results in English
-        ck = Cookie(version=0, name='I4SUserLocale', value='en_US', port=None, port_specified=False, domain='www.google.com', domain_specified=False,domain_initial_dot=False, path='/trends', path_specified=True, secure=False, expires=None, discard=False, comment=None, comment_url=None, rest=None)
+        ck = Cookie(version=0, name='I4SUserLocale', value='en_US', port=None,
+                    port_specified=False, domain='www.google.com',
+                    domain_specified=False, domain_initial_dot=False,
+                    path='/trends', path_specified=True, secure=False,
+                    expires=None, discard=False, comment=None,
+                    comment_url=None, rest=None)
 
         self.cj = CookieJar()
         self.cj.set_cookie(ck)
-        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        self.opener = urllib2.build_opener(
+            urllib2.HTTPCookieProcessor(self.cj))
         self.opener.addheaders = self.headers
 
         # Get all of the login form input values
@@ -76,13 +96,15 @@ class pyGoogleTrendsCsvDownloader(object):
             resp = self.opener.open(self.url_login)
 
             if resp.info().get('Content-Encoding') == 'gzip':
-                buf = StringIO( resp.read())
+                buf = StringIO(resp.read())
                 f = gzip.GzipFile(fileobj=buf)
                 data = f.read()
             else:
                 data = resp.read()
 
-            xmlTree = etree.fromstring(data, parser=html.HTMLParser(recover=True, remove_comments=True))
+            xmlTree = etree.fromstring(
+                data, parser=html.HTMLParser(recover=True,
+                                             remove_comments=True))
 
             for input in find_inputs(xmlTree):
                 name = input.get('name')
@@ -106,7 +128,8 @@ class pyGoogleTrendsCsvDownloader(object):
 
         # Randomized download delay
         if throttle:
-            r = random.uniform(0.5 * self.download_delay, 1.5 * self.download_delay)
+            r = random.uniform(0.5 * self.download_delay, 1.5 *
+                               self.download_delay)
             time.sleep(r)
 
         params = {
@@ -118,17 +141,19 @@ class pyGoogleTrendsCsvDownloader(object):
         r = self.opener.open(self.url_download + params)
 
         # Make sure everything is working ;)
-        if not r.info().has_key('Content-Disposition'):
+        if 'Content-Disposition' not in r.info():
             print "You've exceeded your quota. Continue tomorrow..."
             sys.exit(0)
 
         if r.info().get('Content-Encoding') == 'gzip':
-            buf = StringIO( r.read())
+            buf = StringIO(r.read())
             f = gzip.GzipFile(fileobj=buf)
             data = f.read()
         else:
             data = r.read()
 
-        myFile = open('trends_%s.csv' % '_'.join(['%s-%s' % (key, value) for (key, value) in kwargs.items()]), 'w')
+        myFile = open('trends_%s.csv' % '_'.join(['%s-%s' % (key, value)
+                                                  for (key, value) in
+                                                  kwargs.items()]), 'w')
         myFile.write(data)
         myFile.close()
